1095b66d2242bc5d23adbaeca9a50bfc2a4a5986
Douglas <dfantoni2@gmail.com>
2023-10-05 09:23:18 -0300

    Corrigindo carregamento das imagens

--- app/(routes)/_formPage/HeaderImage.tsx
+++ app/(routes)/_formPage/HeaderImage.tsx
@@ -1,8 +1,7 @@
-import headerImg from '@/public/images/header.png';
 import Image from 'next/image';
 
 interface IHeaderImageProps {}
 
 export default function HeaderImage({}: IHeaderImageProps) {
-	return <Image alt="header da página" fill sizes="480px" src={headerImg} priority />;
+	return <Image alt="header da página" fill sizes="480px" src={'/images/header.png'} priority />;
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: 'export',
-};
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants');
 
-module.exports = nextConfig
+module.exports = async (phase, {defaultConfig}) => {
+	/** @type {import('next').NextConfig} */
+
+	const defaultConfigs = {
+		reactStrictMode: true,
+
+		// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+		skipTrailingSlashRedirect: true,
+
+		// Optional: Change the output directory `out` -> `dist`
+		// distDir: 'dist',
+
+		swcMinify: true,
+	};
+
+	// PRODUCAO OU HOMOLOGACAO
+	if (phase !== PHASE_DEVELOPMENT_SERVER) {
+		return {
+			...defaultConfigs,
+			// Exportação estatica:
+			output: 'export',
+			// Exportação estática não permite a utilização do otimizador
+			images: {
+				unoptimized: true,
+			},
+		};
+	}
+
+	if (phase === PHASE_DEVELOPMENT_SERVER) {
+		return {
+			...defaultConfigs,
+		};
+	}
+
+	return {
+		...defaultConfigs,
+	};
+};
